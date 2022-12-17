5a7f36bf1cebe6b90940cfa5173ec6f18060c50a
Jan Koupil <jan.koupil@gmail.com>
2022-12-17 21:56:01 +0100

    Strat dialog focus and autofind elements

--- Assets/StartMessageBoxController.cs
+++ Assets/StartMessageBoxController.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
 public class StartMessageBoxController : MonoBehaviour
 {
-    public TMP_Text Message;    
-    public Button ContinueBtn;    
-    //public TMP_Text BackText;
+    private TMP_Text _message;    
+    private Button _continueBtn;    
     public bool IsVisible { get; private set; }
+
+    private void Awake()
+    {
+        _message = transform.Find("MessageText").gameObject.GetComponent<TMP_Text>();
+        _continueBtn = transform.Find("ContinueBtn").gameObject.GetComponent<Button>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +27,14 @@ public class StartMessageBoxController : MonoBehaviour
     // Update is called once per frame
     public void Show(int levelNo, string code)
     {
-        Message.text = $"Level: {levelNo}\nCode: {code}";
+        _message.text = $"Level: {levelNo}\nCode: {code}";
         gameObject.SetActive(true);
         IsVisible = true;
+        EventSystem.current.SetSelectedGameObject(transform.Find("ContinueBtn").gameObject);
     }
     public void Hide()
     {
         gameObject.SetActive(false);
         IsVisible = false;
     }
-    //public void ButtonPress()
-    //{
-    //    this.Hide();
-    //}
-
 }
