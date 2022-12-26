bd61726f1ec6550de088a82e1abea47d50458dbb
Jan Koupil <jan.koupil@gmail.com>
2022-12-26 20:38:26 +0100

    Help dialog is a prefab

---
+++ Assets/HelpDialogController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HelpDialogController : MonoBehaviour
+{
+    public Action OnClose = null;
+
+    private GameObject _closeBtn;
+
+    public void Awake()
+    {
+        _closeBtn = transform.Find("CloseHelpBtn").gameObject;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(_closeBtn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCloseBtnClick()
+    {
+        if (OnClose != null)
+            OnClose();
+
+        Destroy(gameObject);
+    }
+
+
+}
